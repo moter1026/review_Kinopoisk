5f364dfd7fbf565049176829b8c076ac67a69a7a
moter1026 <matvej.sidoroff2016@yandex.ru>
2023-11-26 01:53:52 +0400

    Улучшил работу скрипта для пункта 4 второй лабы

--- get_class_return_item.py
+++ get_class_return_item.py
@@ -1,36 +1,26 @@
 import os
 import csv
-import pandas as pd
 
 
 def get_class_return_item(class_name):
     if (os.path.exists("return_path.csv")):
-        lines_of_class = 0
         names_of_files = []
         with open("return_path.csv", "r", encoding="utf-8") as readFile:
             read = csv.DictReader(readFile)
             for i, row in enumerate(read):
                 names_of_files.append(row["Возвращённый путь до файла"])
-                if class_name == row["Класс"]:
-                    lines_of_class = lines_of_class + 1
 
 
         path = None
         file_class = 0
         with open("description_three_random.csv", "r", encoding='utf-8') as csvFile:
             read = csv.DictReader(csvFile)
-            count = 0
-            name_exists = False
             for i, row in enumerate(read, start=0):
                 if row["метка класса"] == class_name:
-                    if count == lines_of_class or name_exists == True:
-                        # Проверка на выдачу до этого данного пути файла
-                        if row["абсолютный путь к файлу"] in names_of_files:
-                            name_exists = True
-                            continue
-                        path = row["абсолютный путь к файлу"]
-                        file_class = row["метка класса"]
-                    count = count + 1
+                    if row["абсолютный путь к файлу"] in names_of_files:
+                        continue
+                    path = row["абсолютный путь к файлу"]
+                    file_class = row["метка класса"]
 
         if path != None:     
             with open("return_path.csv", "a", encoding='utf-8') as TextFile:
